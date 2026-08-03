42c4b477774ae6888613bdee19db1a2e4163263e
David19 <davidleonel911@gmail.com>
2026-08-03 10:13:51 -0500

    Metodos GET para retornar todos o buscar por id

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
-
-
 var app = builder.Build();
 
 
-app.MapGet("/api/test", () =>
+//Productos 
+var inventory = new List<Product>
 {
-    return Results.Ok(new { Mensaje = "Test" });
-});
+    new() {Name="Mouse", Price=12.4m, Stock=8},
+    new() {Name="Keyboard", Price=92.9m, Stock=9},
+    new() {Name="Mousepad", Price=81.4m, Stock=0},
+};
+
+
+    //GET
+//Obtener todos los productos
+app.MapGet("/api/products/", () => inventory);
+
+
 
 app.Run();
+
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+
+}
+
+
+
+
+
+
+

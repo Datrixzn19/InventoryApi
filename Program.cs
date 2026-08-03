04ab085a63be89e9df563558b93e070d07f2aa6a
David19 <davidleonel911@gmail.com>
2026-08-03 11:13:10 -0500

    Metodo POST creado

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ var app = builder.Build();
 //Productos 
 var inventory = new List<Product>
 {
-    new() {Id = 0, Name="Mouse", Price=12.4m, Stock=8},
-    new() {Id = 1, Name="Keyboard", Price=92.9m, Stock=9},
-    new() {Id = 2, Name="Mousepad", Price=81.4m, Stock=0},
+    new() {Id = 1, Name="Mouse", Price=12.4m, Stock=8},
+    new() {Id = 2, Name="Keyboard", Price=92.9m, Stock=9},
+    new() {Id = 3, Name="Mousepad", Price=81.4m, Stock=0},
 };
 
 
@@ -23,7 +23,33 @@ app.MapGet("/api/products/{id:int}", (int id) =>
     return product is null ? Results.NotFound() : Results.Ok(product);
 
 });
+    
+    // POST
+app.MapPost("/api/products/lote", (List<Product> newProducts) =>
+{
+    //Verificar que no venga vacia 
+    if (newProducts.Count() == 0) return Results.BadRequest();
 
+
+
+
+    //Contabilizar los IDs
+    int nextID = inventory.Count()>0 ? inventory.Max(p => p.Id) + 1 : 1;
+    
+    //Verificar que si hayan productos
+
+    //Asignar los IDs
+    foreach (var product in newProducts)
+    {
+        product.Id = nextID;//asignamos el id
+        nextID++;//Aumentamos para el siguiente producto
+
+    }
+    //Add Range agrega los productos de golpe 
+    inventory.AddRange(newProducts);
+    return Results.Ok(newProducts);
+
+});
 
 app.Run();
 

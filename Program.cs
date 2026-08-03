1b33898ed06c73c94bd9cbb271d65ca735aa51d0
David19 <davidleonel911@gmail.com>
2026-08-03 10:27:20 -0500

    Asignacion de IDs a la lista predefinida para buscar productos correctamente

--- Program.cs
+++ Program.cs
@@ -5,16 +5,24 @@ var app = builder.Build();
 //Productos 
 var inventory = new List<Product>
 {
-    new() {Name="Mouse", Price=12.4m, Stock=8},
-    new() {Name="Keyboard", Price=92.9m, Stock=9},
-    new() {Name="Mousepad", Price=81.4m, Stock=0},
+    new() {Id = 0, Name="Mouse", Price=12.4m, Stock=8},
+    new() {Id = 1, Name="Keyboard", Price=92.9m, Stock=9},
+    new() {Id = 2, Name="Mousepad", Price=81.4m, Stock=0},
 };
 
 
     //GET
 //Obtener todos los productos
 app.MapGet("/api/products/", () => inventory);
+//Obtener un producto por id 
+app.MapGet("/api/products/{id:int}", (int id) => 
+{
+    //Seleccionarlo
+    var product = inventory.FirstOrDefault(p => p.Id == id);
+    //Comprobar que exista
+    return product is null ? Results.NotFound() : Results.Ok(product);
 
+});
 
 
 app.Run();

507bfca618f281a27b71ddadf73f313bf7f871b3
David19 <davidleonel911@gmail.com>
2026-08-04 20:00:25 -0500

    Metodo Put terminado

--- Program.cs
+++ Program.cs
@@ -30,26 +30,44 @@ app.MapPost("/api/products/lote", (List<Product> newProducts) =>
     //Verificar que no venga vacia 
     if (newProducts.Count() == 0) return Results.BadRequest();
 
-
-
-
     //Contabilizar los IDs
     int nextID = inventory.Count()>0 ? inventory.Max(p => p.Id) + 1 : 1;
     
-    //Verificar que si hayan productos
-
     //Asignar los IDs
     foreach (var product in newProducts)
     {
         product.Id = nextID;//asignamos el id
         nextID++;//Aumentamos para el siguiente producto
-
     }
+
     //Add Range agrega los productos de golpe 
     inventory.AddRange(newProducts);
     return Results.Ok(newProducts);
+});
+
+
+
+//PUT
+app.MapPut("api/products/{id:int}", (Product productUpdated, int id) =>
+{
+    //Verificamos que no venga vacia
+    if(productUpdated is null) Results.BadRequest();
+
+    //Verificamos que el producto si exista
+    var product = inventory.FirstOrDefault(p => p.Id == id);
+    if (product is null) Results.BadRequest();
+
+    //Actualizamos los campos
+    product.Name = productUpdated.Name;
+    product.Price = productUpdated.Price;
+    productUpdated.Stock = productUpdated.Stock;
+
+    return Results.Ok(productUpdated);
+    //return Results.NoContent(); msj de exito pero sin cuerpo de respuesta 
 
 });
+
+
 
 app.Run();
 
